1276da660cc36c4eb9c90059ce8bdd8257824292
unknown <anisamim94@gmail.com>
2024-08-22 00:10:03 +0600

    initialized admission form

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+     
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routes/Routes';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)

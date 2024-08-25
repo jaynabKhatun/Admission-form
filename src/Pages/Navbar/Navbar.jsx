840cc7a05fd66f5f9ce5b605dc1c4ed5d90990de
unknown <anisamim94@gmail.com>
2024-08-25 18:51:57 +0600

    added responsive header

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -2,10 +2,10 @@
 const Navbar = () => {
   return (
     <div className="navbar bg-[#024987] text-white">
-      <button className="btn btn-ghost text-xl from-neutral-800 italic ">
+      <p className=" md:text-base  italic ">
         An online admission form makes enrollment quick and easy with just a few
         clicks
-      </button>
+      </p>
     </div>
   );
 };
